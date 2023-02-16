be088ce74f50dca40796ad02aa32f90829683496
Mark <not.markyun@gmail.com>
2023-02-16 09:12:30 -0800

    reset

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require('express')
 
-const setupTables = require('./middleware/setupTables')
-const tables = require('./models/tables')
+// const setupTables = require('./middleware/setupTables')
+// const tables = require('./models/tables')
 
 
 const app = express()
-const { Client } = require('pg')
-const client = new Client({
-  port : 5432,
-  user: 'mark',
-  password: 'mark',
-  database: 'products'
-})
+// const { Client } = require('pg')
+// const client = new Client({
+//   port : 5432,
+//   user: 'mark',
+//   password: 'mark',
+//   database: 'products'
+// })
 
-const createSchema = `CREATE SCHEMA products`
+// const createSchema = `CREATE SCHEMA products`
 
-client.connect().then(() => {
-  console.log('postgres connected on 5000')
+// client.connect().then(() => {
+//   console.log('postgres connected on 5000')
 
-  return setupTables(client, tables);
-  })
-  .then(() => {
-    console.log('queries created');
-  });
+//   return setupTables(client, tables);
+//   })
+//   .then(() => {
+//     console.log('queries created');
+//   });
 
 app.listen(3020)
